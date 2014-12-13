679d3a5ede6fc5394ba9e0577de80e4c9305f3ab
transcedentalia <transcedentalia@gmail.com>
2014-12-13 14:20:53 +0200

    Updated PotD13122014

--- PotD/src/PotD13122014.py
+++ PotD/src/PotD13122014.py
@@ -1,9 +1,11 @@
 __author__ = 'alina'
 
+from collections import OrderedDict
+
 def getAbbrevation(word):
     if word:
         return word[0] + str(len(word) - 2) + word[len(word) - 1]
-     return
+    return None
 
 def buildHash():
     f = open('words', 'r')
@@ -14,12 +16,13 @@ def buildHash():
         word = line[:-1]
         abbreviation = getAbbrevation(word)
 
-        if not abbreviation in wordsHash:
-            wordsHash[abbreviation] = [word]
-        else:
-            t = wordsHash[abbreviation]
-            t.append(word)
-            wordsHash[abbreviation] = t
+        if abbreviation:
+            if not abbreviation in wordsHash:
+                wordsHash[abbreviation] = [word]
+            else:
+                t = wordsHash[abbreviation]
+                t.append(word)
+                wordsHash[abbreviation] = t
 
     f.close()
     return wordsHash
@@ -27,15 +30,16 @@ def buildHash():
 def isDulicate(wordsHash, word):
     abbreviation = getAbbrevation(word)
 
-    if abbreviation in wordsHash:
+    if abbreviation:
+        if abbreviation in wordsHash:
+            if len(wordsHash[abbreviation]) > 1:
+                return 1
+            return 0
 
-        if len(wordsHash[abbreviation]) > 1:
-            return 1
-        return 0
-    return 0
+    return -1
 
 if __name__ == "__main__":
     wordsHash = buildHash()
-    print wordsHash
-    print isDulicate(wordsHash, '')
+    print OrderedDict(sorted(wordsHash.items()))
+    print isDulicate(wordsHash, 'ayyyb')
 
